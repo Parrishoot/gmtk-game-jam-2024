49297e927b6a47846df8f108b38ec450959b7caa
Parrishoot <mparrish@u.rochester.edu>
2024-08-19 17:33:17 -0400

    Updating cards

--- Assets/Scripts/MoveCardController.cs
+++ Assets/Scripts/MoveCardController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MoveCardController : MonoBehaviour, ISelectHandler
+public class MoveCardController : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private TMP_Text titleText;
 
     [SerializeField]
     private TMP_Text descriptionText;
+
+    [SerializeField]
+    private TMP_Text costText;
 
     [SerializeField]
     private Selectable selectable;
@@ -25,9 +29,12 @@ public class MoveCardController : MonoBehaviour, ISelectHandler
 
         titleText.text = actionMetadata.Name;
         descriptionText.text = actionMetadata.GetDescription(characterManager.GetAdjacencyBonuses());
+        costText.text = actionMetadata.Cost.ToString();
 
         this.characterManager = characterManager;
         this.actionMetadata = actionMetadata;
+
+        
     }
 
     public void StartAction() {
@@ -38,5 +45,15 @@ public class MoveCardController : MonoBehaviour, ISelectHandler
     public void OnSelect(BaseEventData eventData)
     {
         StartAction();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one * 1.125f, .1f).SetEase(Ease.InOutSine);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one, .1f).SetEase(Ease.InOutSine);
     }
 }
